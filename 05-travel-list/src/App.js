269b639e2e4d956ad00393244a0273584901de2d
George Dimitrov <georgiangelovdimitrov92@gmail.com>
2024-02-28 17:53:40 +0200

    [add] Calculating status using Derived State

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -14,17 +14,27 @@ function App() {
     }
 
     function handleDeleteItem(itemId) {
-        setItems((items) => (items.filter((i) => i.id !== itemId)))
+        setItems((items) => items.filter((i) => i.id !== itemId));
+    }
+
+    function handleTogglePacked(itemId) {
+        setItems((items) =>
+            items.map((i) =>
+                i.id === itemId ? { ...i, packed: !i.packed } : i
+            )
+        );
     }
 
     return (
         <div className="app">
             <Logo />
             <Form onAddItem={hadleAddItems} />
-            <PackingList items={items}
-            onDeleteItem={handleDeleteItem}
+            <PackingList
+                items={items}
+                onDeleteItem={handleDeleteItem}
+                onTogglePacking={handleTogglePacked}
             />
-            <Stats />
+            <Stats items={items} />
         </div>
     );
 }
@@ -77,38 +87,60 @@ function Form({ onAddItem }) {
     );
 }
 
-function PackingList({ items , onDeleteItem }) {
+function PackingList({ items, onDeleteItem, onTogglePacking }) {
     return (
         <div className="list">
             <ul>
                 {items.map((item) => (
-                    <Item item={item} key={item.id} onDeleteItem={onDeleteItem} />
+                    <Item
+                        item={item}
+                        key={item.id}
+                        onDeleteItem={onDeleteItem}
+                        onTogglePacking={onTogglePacking}
+                    />
                 ))}
             </ul>
         </div>
     );
 }
 
-function Item({ item, onDeleteItem }) {
+function Item({ item, onDeleteItem, onTogglePacking }) {
     return (
         <li>
+            <input
+                type="checkbox"
+                value={item.packed}
+                onChange={() => onTogglePacking(item.id)}
+            ></input>
             <span style={item.packed ? { textDecoration: "line-through" } : {}}>
                 {item.description} - {item.quantity}
             </span>
-            <button 
-            onClick={() => onDeleteItem(item.id)}
-            >❌</button>
+            <button onClick={() => onDeleteItem(item.id)}>❌</button>
         </li>
     );
 }
 
-function Stats() {
+function Stats({ items }) {
+    if (!items.length)
+        return (
+            <p className="stats">
+                <em>Start adding some items to your pacing list 🚀</em>
+            </p>
+        );
+
+    const itemsCount = items.length;
+    const packedItems = items.filter((i) => i.packed === true).length;
+    const percentOfPackedItems = Math.round((packedItems / itemsCount) * 100);
+
     return (
-        <em className="stats">
-            <footer>
-                🎒 You have X items on your list, and you already packed X (X%)
-            </footer>
-        </em>
+        <footer className="stats">
+            <em>
+                {percentOfPackedItems === 100
+                    ? "You got everything! Ready do go! ✈"
+                    : `🎒 You have ${itemsCount} items on your list, and you already
+                packed ${packedItems} (${percentOfPackedItems}%)`}
+            </em>
+        </footer>
     );
 }
 
