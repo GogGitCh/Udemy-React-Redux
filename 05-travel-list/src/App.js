0651c9a4b9d5fd976ff4c47300b4ba25d380070c
George Dimitrov <georgiangelovdimitrov92@gmail.com>
2024-02-28 19:06:06 +0200

    [add] sorting by input, description and if packed

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -88,10 +88,27 @@ function Form({ onAddItem }) {
 }
 
 function PackingList({ items, onDeleteItem, onTogglePacking }) {
+    const [sortBy, setSortBy] = useState("input");
+
+    let sortedItems;
+
+    if (sortBy === "input") {
+        sortedItems = items;
+    }
+    if (sortBy === "desc") {
+        sortedItems = items.slice().sort((a, b) => a.description.localeCompare(b.description));
+    }
+    if (sortBy === "packed") {
+        // sortedItems = items.slice().filter((i) => i.packed !== false);
+        // or
+        sortedItems = items.slice().sort((a,b)=> Number(a.packed) - Number(b.packed));
+
+    }
+
     return (
         <div className="list">
             <ul>
-                {items.map((item) => (
+                {sortedItems.map((item) => (
                     <Item
                         item={item}
                         key={item.id}
@@ -100,6 +117,17 @@ function PackingList({ items, onDeleteItem, onTogglePacking }) {
                     />
                 ))}
             </ul>
+
+            <div className="actions">
+                <select
+                    value={sortBy}
+                    onChange={(e) => setSortBy(e.target.value)}
+                >
+                    <option value="input"> Sort by input order</option>
+                    <option value="desc"> Sort by description order</option>
+                    <option value="packed"> Sort by packed stats</option>
+                </select>
+            </div>
         </div>
     );
 }
