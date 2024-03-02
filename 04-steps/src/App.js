d3f920a81d4f8a67ce6bd7c977e6ead16ad63e51
George Dimitrov <georgiangelovdimitrov92@gmail.com>
2024-03-02 11:21:09 +0200

    [refact] Some more refactoring using children

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -52,9 +52,9 @@ function App() {
                         <div className={step >= 3 ? "active" : ""}>3</div>
                     </div>
 
-                    <p className="message">
-                        Step {step} : {messages[step - 1]}
-                    </p>
+                    <StepMessage key={step} step={step}>
+                        {messages[step - 1]}
+                    </StepMessage>
 
                     <div className="buttons">
                         {/* {inactivePrev && ( */}
@@ -80,6 +80,14 @@ function App() {
                 </div>
             )}
         </>
+    );
+}
+
+function StepMessage({ step, children }) {
+    return (
+        <div className="message">
+            <h3>Step {step}:</h3> {children}
+        </div>
     );
 }
 
